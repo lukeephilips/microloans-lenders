41cc525672754e48030f46c34cefc088cdc48dbe
Luke Philips <luke.e.philips@gmail.com>
2017-01-27 16:15:13 -0800

    updated readme with next steps on project

--- src/app/lender.model.ts
+++ src/app/lender.model.ts
@@ -1,4 +1,4 @@
 export class Lender {
   constructor(public name: string, public type: string, public image: string, public accountBalance: number){}
-  portfolio: any = {"your first loan":1}
+  portfolio: any[] = []
 }
